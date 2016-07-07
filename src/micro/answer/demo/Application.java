2c21d3f4a18247ea540cc3cefa6e78531f1dc910
MicroAnswer <374288225@qq.com>
2016-07-07 23:25:58 +0800

    Update Application.java

--- src/micro/answer/demo/Application.java
+++ src/micro/answer/demo/Application.java
@@ -6,6 +6,7 @@ public class Application {
 		System.out.println("这是Eclipse项目,用于发布到GitHub的测试");
 		//TODO 新增测试TODO 
 		System.out.println("哇咔咔,这个竟然也可以");
+		System.out.println("玩玩没想到,哈哈,这是一段在GitHub上面写下的代码");
 	}
 
 }
